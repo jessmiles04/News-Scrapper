501d139928de939bcdf3a281eb0cdfa67f639c41
Jess Miles <jess_miles04@yahoo.com>
2017-06-11 22:33:18 -0400

    Was working until added send, now claiming extra semicolon on 137

--- server.js
+++ server.js
@@ -40,32 +40,37 @@ app.get('/', function(req, res) {
 
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  request("http://www.iflscience.com/", function(error, response, html) {
+  request("http://bookriot.com/", function(error, response, html) {
+      console.log(html);
     var $ = cheerio.load(html);
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("h3 title").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children("a").text();
-      result.link = $(this).children("a").attr("href");
-      var entry = new Article(result);
+    var result = [];
+    $("a.trackable").each(function(i, element) {
+        var title = $(this).text();
+        var link = $(element).attr("href");
+        result.push({
+            title: title,
+            link: link
+        });
+      //result.title = $(this).children("a").text();
+      //result.link = $(this).children("a").attr("href");
+      //var entry = new Article(result);
       // Now, save that entry to the db
-      entry.save(function(err, doc) {
+    entry.save(function(err, doc) {
         // Log any errors
         if (err) {
-          console.log(err);
-        }
+         console.log(err);
+       }
         // Or log the doc
         else {
-          console.log(doc);
+         console.log(doc);
         }
+        console.log(result);
       });
     });
   });
   // Tell the browser that we finished scraping the text
   res.send("Scrape Complete");
-});
+// });
 // This will get the articles we scraped from the mongoDB
 app.get("/articles", function(req, res) {
   Article.find({}, function(error, doc) {
@@ -108,7 +113,8 @@ app.post("/articles/:id", function(req, res) {
     }
     // Otherwise
     else {
-      // Use the article id to find and update it's note
+   
+   // Use the article id to find and update it's note
       Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
       // Execute the above query
       .exec(function(err, doc) {
@@ -124,7 +130,8 @@ app.post("/articles/:id", function(req, res) {
     }
   });
 });
+
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(3000, function(){
+  console.log("App running on port 3000!")
 });
